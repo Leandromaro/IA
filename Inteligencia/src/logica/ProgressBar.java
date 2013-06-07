3695cb6203f896c0b9dfe1678cabd57c9d8eea54
Leandro <Leandro@Juancho-PC>
2013-06-07 19:15:08 -0300

    Hice la clase ProgressBar para que muestre el avanze

---
+++ Inteligencia/src/logica/ProgressBar.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ProgressBar {
+
+    static JFrame ventana;
+    static JButton boton;
+    static JProgressBar barra;
+    private static Episodio[] episodios;
+
+    public static Episodio[] getEpisodios() {
+        return episodios;
+    }
+    public ProgressBar() {
+//        QMat MQ=matrizQ;
+//        Estado EF=estadoFinal;
+//        Politica P=politica;
+//        RMat R=mat;
+        ventana=new JFrame("JProgressBar");
+        boton = new JButton("Comenzar");
+        barra=new JProgressBar(0,100);
+        
+        boton.addActionListener(new Escucha());
+        
+        ventana.setLayout(new FlowLayout());
+        ventana.add(barra);
+        ventana.add(boton);
+        ventana.pack();
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        
+        
+        
+        
+    }
+    public static void main(String[] args){
+        new ProgressBar();
+    }
+    public static class Escucha implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            new Thread(new Hilo()).start();
+            
+        }
+    }
+    public static class Hilo implements Runnable{
+
+        
+        public void run() {
+        episodios= new Episodio[Configuraciones.getCantEpisodios()];
+        int i=0;
+           while((i<100)){
+//                episodios[i]= new Episodio(MQ,EF,P,R,i);
+                barra.setValue(i);
+                barra.repaint();
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ProgressBar.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                
+                i++;
+           }
+        System.out.println(episodios[Configuraciones.cantEpisodios-1].getMatrizQ());
+        
+        }
+
+        
+       
+   
+
+    
+        }
+}
+    
+
+
