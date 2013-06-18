a5b4fb0859d16bbfb0020a84273a615398f4946e
Leandro <Leandro@Leandro-PC>
2013-06-18 11:02:08 -0300

    Solucionado los bloqueos

--- Inteligencia/src/logica/Configuraciones.java
+++ Inteligencia/src/logica/Configuraciones.java
@@ -18,7 +18,7 @@ public class Configuraciones {
     private static double valorExcelente = 5;
     private static double valorFinal = 100;
     private static int dimension = 3; 
-    private static int valorPorDefectoMatQ= 50;//valor "optimo" para que no entre en bucle con EGreedy
+    private static int valorPorDefectoMatQ= 100;//valor "optimo" para que no entre en bucle con EGreedy
     public static int filaI=0;
     public static int filaF=0;
     public static int colI;
